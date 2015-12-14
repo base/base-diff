7b5d297dfa5fc7d91ed11b3485e5f17b1fe47df1
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-14 03:45:27 -0500

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,11 @@
 'use strict';
 
+var diff = require('./');
 var through = require('through2');
 var gulp = require('gulp');
-var diff = require('./');
-// var mocha = require('gulp-mocha');
-// var istanbul = require('gulp-istanbul');
-// var eslint = require('gulp-eslint');
-
-// var lint = ['gulpfile.js', 'index.js', 'lib/*.js', 'test/*.js'];
-
-function append(str) {
-  return through.obj(function(file, enc, next) {
-    file.contents = new Buffer(file.contents.toString() + str);
-    next(null, file);
-  });
-}
-
-function prepend(str) {
-  return through.obj(function(file, enc, next) {
-    file.contents = new Buffer(str + file.contents.toString());
-    next(null, file);
-  });
-}
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var eslint = require('gulp-eslint');
 
 gulp.task('diff', function () {
   return gulp.src('fixtures/**/*')
@@ -39,22 +23,37 @@ gulp.task('diff', function () {
     .pipe(gulp.dest('actual/'));
 });
 
-// gulp.task('coverage', function () {
-//   return gulp.src(lint)
-//     .pipe(istanbul())
-//     .pipe(istanbul.hookRequire());
-// });
+gulp.task('coverage', function () {
+  return gulp.src('*.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
 
-// gulp.task('mocha', ['coverage'], function () {
-//   return gulp.src('test/*.js')
-//     .pipe(mocha({reporter: 'spec'}))
-//     .pipe(istanbul.writeReports());
-// });
+gulp.task('mocha', ['coverage'], function () {
+  return gulp.src('test.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports());
+});
 
-// gulp.task('eslint', function () {
-//   return gulp.src(lint)
-//     .pipe(eslint())
-// });
+gulp.task('eslint', function () {
+  return gulp.src('*.js')
+    .pipe(eslint())
+});
 
-// gulp.task('default', ['mocha', 'eslint']);
 gulp.task('default', ['diff']);
+
+
+
+function append(str) {
+  return through.obj(function(file, enc, next) {
+    file.contents = new Buffer(file.contents.toString() + str);
+    next(null, file);
+  });
+}
+
+function prepend(str) {
+  return through.obj(function(file, enc, next) {
+    file.contents = new Buffer(str + file.contents.toString());
+    next(null, file);
+  });
+}
